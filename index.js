f5b3cba84dcd87c58d875871dfdd93b338c35f51
Liam Dio <44742525+DioChuks@users.noreply.github.com>
2023-07-10 18:00:12 +0100

    added firebase configuration

---
+++ index.js
@@ -0,0 +1,36 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js'
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js'
+const appSettings = {
+    databaseURL: "https://cart-playground-34b8b-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDb = ref(database, "movies")
+const shoppingListInDb = ref(database, "shoppingList")
+const inputFieldEl = document.getElementById('input-field')
+const addButtonEl = document.getElementById('add-button')
+const shoppingListEl = document.getElementById('shopping-list')
+
+addButtonEl.addEventListener('click', ()=>{
+    let inputValue = inputFieldEl.value
+    if(inputValue === ' ' || inputValue === ''){
+        inputFieldEl.value = "";
+        return inputFieldEl.placeholder = "Enter an item";
+    }
+    push(shoppingListInDb, inputValue)
+    clearInputFieldEl()
+    appendItemToShoppingListEl(inputValue)
+})
+function clearInputFieldEl(){
+    inputFieldEl.value = "";
+}
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
+
+let scrimbaUsers = {
+    "i1": "user@scrimba.com",
+    "i2": "user2@scrimba.com",
+    "i3": "user3@scrimba.com"
+}
+console.log(Object.entries(scrimbaUsers))
