5e58428a51a485bc8631191f401b086cedd4c4ee
Liam Dio <44742525+DioChuks@users.noreply.github.com>
2023-07-11 16:19:17 +0100

    added condition for null value

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js'
-import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js'
+import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js'
 const appSettings = {
     databaseURL: "https://cart-playground-34b8b-default-rtdb.europe-west1.firebasedatabase.app/"
 }
@@ -20,24 +20,35 @@ addButtonEl.addEventListener('click', ()=>{
     clearInputFieldEl()
 })
 onValue(shoppingListInDb, function(snapshot) {
-    let shoppingItemsArray = Object.entries(snapshot.val())
-    clearShoppingList()
-    for (let q = 0; q < shoppingItemsArray.length; q++) {
-        let currentItem = shoppingItemsArray[q];
-        let currentItemID = currentItem[0]
-        let currentItemValue = currentItem[1]
-        appendItemToShoppingListEl(currentItemValue)
+    if(snapshot.exists()){
+        let shoppingItemsArray = Object.entries(snapshot.val())
+        clearShoppingList()
+        for (let q = 0; q < shoppingItemsArray.length; q++) {
+            let currentItem = shoppingItemsArray[q];
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            appendItemToShoppingListEl(currentItem)
+        }
+    }else{
+        shoppingListEl.innerHTML = "No items here... yet"
     }
 })
+
 function clearShoppingList(){
     shoppingListEl.innerHTML = ""
 }
 function clearInputFieldEl(){
     inputFieldEl.value = "";
 }
-function appendItemToShoppingListEl(itemValue) {
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
     let newEl = document.createElement("li")
-    newEl.textContent = `${itemValue}`
+    newEl.textContent = itemValue
+    newEl.addEventListener('click', ()=>{
+        let exactLocationOfItemInDb = ref(database, `shoppingList/${itemID}`)
+        remove(exactLocationOfItemInDb)
+    })
     shoppingListEl.append(newEl)
 
 }
